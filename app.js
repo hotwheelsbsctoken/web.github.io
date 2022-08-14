6524af4ab6c4ef2518e3d72af022eb4ff03b83be
hotwheelsbsctoken <hotwheelsbsctoken@hotmail.com>
2022-08-14 00:34:48 +0000

    website

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ particlesJS('particles-js',
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 10,
+          "speed": 100,
           "size_min": 0.1,
           "sync": false
         }
@@ -71,7 +71,7 @@ particlesJS('particles-js',
         "enable": true,
         "speed": 3,
         "direction": "none",
-        "random": false,
+        "random": true,
         "straight": false,
         "out_mode": "out",
         "attract": {
